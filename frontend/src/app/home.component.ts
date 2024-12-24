dec38d60645bad6eff31031ce0d588de7119143b
Travis-Berthrong <33703020+Travis-Berthrong@users.noreply.github.com>
2024-12-23 18:58:27 -0800

    feat: added docker compose for app

--- frontend/src/app/home.component.ts
+++ frontend/src/app/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [RouterLink]
 })
 export class HomeComponent {}
